4419bb0817f4b01e5aab038bc96b9f3c323bdf95
Justin Funston <jfunston@gmail.com>
2025-06-09 12:30:34 -0600

    Update LumkiServiceProvider.php

--- src/LumkiServiceProvider.php
+++ src/LumkiServiceProvider.php
@@ -90,7 +90,7 @@ class LumkiServiceProvider extends ServiceProvider
             return
             Blade::compileString(
                 '@can(\'' . config('lumki.lumkiPermission') . '\') '
-                    .'<div class="block px-4 py-2 text-xs text-gray-400">Lumki</div>'
+                    .'<div class="block px-4 py-2 text-xs text-gray-400">User Management</div>'
 
                     .'<a href="{{ route(\'lumki.users.index\') }}" class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">'
                         .'{{ __(\'lumki::ui.users\') }}'
